93bab684780c4c39f2f7d048d3967152c1f96ba2
Jasper Sone <jaspersone@gmail.com>
2012-03-22 23:53:34 -0500

    Broken encrypt, but everything up until that point is ok

--- RSA.java
+++ RSA.java
@@ -1,4 +1,5 @@
 import java.io.*;
+import java.util.Arrays;
 
 public class RSA {
 
@@ -16,7 +17,7 @@ public class RSA {
 			String inputFileName = args[1];
 			String keyFile = args[2];
 			String outputFileName = args[3];
-			
+
 			if (command.equals("encrypt")) {
 				encrypt(inputFileName, keyFile, outputFileName);
 			} else if (command.equals("decrypt")) {
@@ -26,7 +27,7 @@ public class RSA {
 			}
 		}
 	}
-	
+
 	public static String generateKey(long p, long q){
 		assert(p > 0 && q > 0);
 		assert(isPrime(p) && isPrime(q));
@@ -40,7 +41,6 @@ public class RSA {
 		}
 		assert ((e > 1) && (phi % e != 0));
 		String result = n + " " + e + " " + d;
-		System.out.println(result);
 		return result;
 	}
 
@@ -54,41 +54,41 @@ public class RSA {
 		}
 		return true;
 	}
-	
+
 	private static long findE(long startE, long phi){
 		while(phi % startE == 0){
 			startE++;		
 		}
 		return startE;
 	}
-	
+
 	/**
 	 * input: phi value and e value where e is a public key
 	 * output: d, where d is the private key
 	 */
-	private static long euclid(long phi, long e, long n){
+	protected static long euclid(long phi, long e, long n){
 		assert(e > 1);
 		assert(isPrime(e));
 		assert(gcd(phi, e) == 1);
 		long d;
 		long[] top		= {1, 0, phi};
 		long[] bottom 	= {0, 1, e};
-		
-		while (bottom[2] != 1) {
+
+		while (bottom[2] > 1) {
 			long quotient = top[2] / bottom[2];
 			long[] newBottom = new long[3];
 			for(int i = 0; i < 3; i++) {
 				newBottom[i] = top[i] - quotient * bottom[i];
 			}
-			bottom = newBottom;
 			top = bottom;
+			bottom = newBottom;
 			if (bottom[2] < 1) {
-				System.out.println(">>>> Oh shit, bottom[2] below 1");
+				System.out.println(">>>> Oh shit, bottom[2] below 1, value = " + bottom[2]);
 			}
 		}
 		assert (bottom[2]) > 0;
 		assert (bottom[0] * phi + bottom[1] * e == 1);
-		
+
 		d = bottom[1];
 		if (d > n) {
 			while (d > n) {
@@ -104,22 +104,160 @@ public class RSA {
 		assert (gcd(d, phi) == 1); // assert d is coprime of phi
 		return d;
 	}
-	
+
 	/**
 	 * input: given two numbers left and right
 	 * output: returns the greatest common denominator
 	 */
-	private static long gcd(long left, long right) {
+	protected static long gcd(long left, long right) {
 		// TODO: write this
-		return 1;
+		if (left == 0) {
+			return right;
+		}
+		while (right != 0) {
+			if (left > right) 	left  = left - right;
+			else 				right = right - left;
+		}
+		return left;
 	}
-	
-	public static void encrypt(String inputFileName, String keyFile, String outputFileName){
-	
+	/**
+	 * Fast Exponentiation
+	 * @param base
+	 * @param exponent
+	 * @param modVal
+	 * @return returns (base^exponent) % modVal
+	 */
+	protected static long fastExponentiation(long base, long exponent, long modVal) {
+		String binary = Long.toBinaryString(exponent);
+		long c = 1; // seed value for result
+		for (int i = 0; i < binary.length(); i++) {
+			c = ((c * c) % modVal);
+			if(binary.charAt(i) == '1') {
+				c = (c * base) % modVal;
+			}
+		}
+		return c;
 	}
-	
+
+	protected static long[] getNEDValues(String keyFile) {
+		String[] temp = keyFile.split("\\s+"); // delimits on whitespace or groups of whitespace
+		assert(temp.length == 3); // make sure the given keyFile is of proper length
+		long[] result = new long[temp.length];
+		for (int i = 0; i < temp.length; i++) {
+			result[i] = Long.valueOf(temp[i]);
+		}
+		return result;
+	}
+
+	public static void encrypt(String inputFileName, String keyFile, String outputFileName) {
+		// acquire n, e, d values
+		long[] ned = getNEDValues(keyFile);
+		boolean eofFound = false;
+		// Get file set up
+		try {
+			FileInputStream f = new FileInputStream(inputFileName);
+			FileOutputStream outf = new FileOutputStream(outputFileName);
+			// Pull sets of 3 from f and encrypt them
+			int[] triple = new int[3];
+			while(!eofFound) {
+				for (int i = 0; i < 3; i++) {
+					int temp = f.read();
+					if (temp != -1) {
+						triple[i] =  ((Integer) temp).byteValue();
+					} else {
+						while(i < 3) {
+							triple[i] = -1;
+							i++;
+						}
+						f.close();					
+					}
+				}
+				System.out.println("Sending to write: " + Arrays.toString(triple));
+				eofFound = writeCurrentTuple(3, ned, triple, outf);
+			}
+			if (eofFound) {
+				System.out.println("Found EOF.");
+			}
+			f.close();
+		} catch (FileNotFoundException ex) {
+			// TODO Auto-generated catch block
+			System.out.println("There was an error opening the file " + inputFileName);
+			ex.printStackTrace();
+		} catch (IOException r) {
+			// TODO Auto-generated catch block
+			System.out.println("IO Exception occurred while accessing f.");
+			r.printStackTrace();
+		}
+	}
+
+	/**
+	 * Write Current Tuple to File
+	 * @param limit: # of bytes to write at one time
+	 * @param ned: n = limit, e = public key, d = private key
+	 * @param currTriple: set of numbers to pack and encode
+	 * @param f: output file object
+	 * @return true if end of file is found, false otherwise
+	 */
+	protected static boolean writeCurrentTuple(int limit, long[] ned, int[] currTriple, FileOutputStream f) {
+		assert(ned.length == limit);
+		assert(currTriple.length == limit);
+		assert(f != null);
+		boolean eofFound = false;
+		long n = ned[0];
+		long e = ned[1];
+		long d = ned[2];
+		int word = 0;
+		for (int i = 0; i < limit; i++) {
+			int temp = ((Integer) currTriple[i]).byteValue();
+			if (currTriple[i] == -1) {
+				System.out.println("Inside write, found EOF");
+				eofFound = true;
+				break;
+			} else if (i == limit - 1) {
+				word += temp;
+			} else {
+				word += temp << ((limit - (i + 1)) * 8);
+			}
+		}
+		System.out.println("Word to encrypt:  " + Integer.toBinaryString(word));
+		assert(word < ((Long) n).byteValue()); // insure this message is encode safe
+		// encrypt message
+		long encryptedMessage = fastExponentiation(word, e, n);
+		String em = Long.toBinaryString(encryptedMessage);
+		System.out.println("Encrypted Message:" + em);
+		int length = em.length();
+		assert(length <= 32); // make sure the number did not overflow our container
+		byte[] output = new byte[4];
+		// grab first chunk
+		int offset = 8 - (32 - length);
+		assert(offset >= 0);
+		if (offset == 0) {
+			output[0] = 0;
+		} else {
+			output[0] = ((Integer) Integer.parseInt(em.substring(0, offset))).byteValue();
+		}
+		// grab rest of chunks
+		for (int i = 1; i < limit; i++) {
+			output[i] = ((Integer) Integer.parseInt(em.substring(offset, offset + 8))).byteValue();
+			offset += 8;
+		}
+		// break encrypted message into 4 bytes, then write to f
+		for (byte b : output) {
+			try {
+				if (b != 0) {
+					f.write(b);
+				}
+			} catch (IOException badf) {
+				// TODO Auto-generated catch block
+				System.out.println("Problems writing to file");
+				badf.printStackTrace();
+			}
+		}
+		return eofFound;
+	}
+
 	public static void decrypt(String inputFileName, String keyFile, String outputFileName){
-		
+
 	}
-	
+
 }
--- RSATest.java
+++ RSATest.java
@@ -1,9 +1,9 @@
 import static org.junit.Assert.*;
-
 import org.junit.Test;
 
-public class RSATest {
 
+public class RSATest {
+	
 	@Test
 	public void testMain() {
 		fail("Not yet implemented");
@@ -14,16 +14,81 @@ public class RSATest {
 		String t1 = RSA.generateKey(61, 53);
 		assertEquals("Test t1: <p = 61> <q = 53>", t1, "3233 7 1783");
 	}
+
+	@Test
+	public void testEuclid1() {
+		long phi = (47 - 1) * (59 - 1);
+		long testE = 17;
+		long testN = 2773;
+		long expectedD = 157;
+		long testResult = RSA.euclid(phi, testE, testN);
+		assertEquals(expectedD, testResult);
+	}
 	
 	@Test
-	public void testGenerateKey2() {
-		String t2 = RSA.generateKey(47, 59);
-		assertEquals("Test t2: <p = 47> <q = 59>", t2, "2773 17 157");
+	public void testFastExponentiation1() {
+		long base = 4;
+		long expo = 35;
+		long mod = 11;
+		long testResult = RSA.fastExponentiation(base, expo, mod);
+		long expectedResult = 1;
+		assertEquals(testResult, expectedResult);
+	}
+	
+	@Test
+	public void testFastExponentiation2() {
+		long base = 12348;
+		long expo = 7829;
+		long mod = 347;
+		long testResult = RSA.fastExponentiation(base, expo, mod);
+		long expectedResult = 198;
+		assertEquals(testResult, expectedResult);
+	}
+	
+	@Test
+	public void testGCD1() {
+		long testResult = RSA.gcd(4389, 17347);
+		long expectedResult = 209;
+		assertEquals(testResult, expectedResult);
+	}
+	
+	@Test
+	public void testGCD2() {
+		long testResult = RSA.gcd(43897, 17347);
+		long expectedResult = 1;
+		assertEquals(testResult, expectedResult);
+	}
+	
+	@Test
+	public void testGCD3() {
+		long testResult = RSA.gcd(10, 20);
+		long expectedResult = 10;
+		assertEquals(testResult, expectedResult);
+	}
+	
+	@Test
+	public void testGetNEDValues1() {
+		String testString = "10 20 50";
+		long[] expectedResult = {10, 20, 50};
+		long[] testResult = RSA.getNEDValues(testString);
+		for (int i = 0; i < testResult.length; i++) {
+			assertEquals(testResult[i], expectedResult[i]);
+		}
+	}
+	@Test
+	public void testGetNEDValues2() {
+		String testString = "0 1 1010020348";
+		long[] expectedResult = {0, 1, 1010020348};
+		long[] testResult = RSA.getNEDValues(testString);
+		for (int i = 0; i < testResult.length; i++) {
+			assertEquals(testResult[i], expectedResult[i]);
+		}
 	}
 
 	@Test
 	public void testEncrypt() {
-		fail("Not yet implemented");
+		RSA.encrypt("test1.txt", "16777219 1181 184541", "mytest1.enc");
+		assertEquals(1, 1);
 	}
 
 	@Test
